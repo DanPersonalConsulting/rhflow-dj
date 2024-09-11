b08987cc9b942dce585d576e479b95a3bc2dec64
Danilo Lorenzi <danilo.lorenzi@gmail.com>
2024-09-11 19:48:38 -0300

    add gunicorn dependecy. Also adjust deploy.sh to use gunicorn

--- deploy.sh
+++ deploy.sh
@@ -23,15 +23,16 @@ sudo mkdir -p $WWW_ROOT
 
 
 for tenant in "${tenants[@]}"; do
+    cd /home/ubuntu
     echo copying files to "${tenant}"
     sudo mkdir -p  $WWW_ROOT/$tenant
+    sudo cp -r *  $WWW_ROOT/$tenant
     cd $WWW_ROOT/$tenant
 
     echo "creating virtual environment for ${tenant}"
     sudo chown -R ubuntu:ubuntu $WWW_ROOT/$tenant
     python3 -m venv venv
     source venv/bin/activate
-    sudo cp -r *  $WWW_ROOT/$tenant
 
     echo "installing dependencies"
     pip install -r requirements.txt
@@ -59,10 +60,9 @@ EOF"
     sudo sed -i "s/DB_NAME=.*/DB_NAME=${tenant}/g" .env
 
     echo "starting gunicorn for ${tenant}"
-    cd $WWW_ROOT/$tenant/rhflow
-    gunicorn --workers 3 --bind unix:$WWW_ROOT/$tenant/app.sock wsgi:application --user ubuntu --group ubuntu --log-file $WWW_ROOT/$tenant/gunicorn.log --timeout 90 --daemon
+    cd $WWW_ROOT/$tenant
+    gunicorn --workers 3 --bind unix:$WWW_ROOT/$tenant/app.sock rhflow.wsgi:application --user ubuntu --group ubuntu --log-file $WWW_ROOT/$tenant/gunicorn.log --timeout 90 --daemon
     echo "${tenant} deployed successfully"
-    cd $WWW_ROOT
 done
 
 echo "restarting nginx"
