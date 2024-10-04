7f93e1d7e19d3c80f8f861fbe3602fa33299b417
alsox369 <alccpe@yahoo.com.br>
2024-10-03 23:05:24 -0300

    Refatoração header principal

--- rhflow/urls.py
+++ rhflow/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app.base.urls', namespace='base')),
-    path('accounts/', include('app.accounts.urls', namespace='account')),
+    path('', include('app.accounts.urls', namespace='account')),
     path('', include('app.empresa.urls', namespace='empresa')),
     path('funcionarios/', include('app.funcionario.urls', namespace='funcionario')),
 ]
